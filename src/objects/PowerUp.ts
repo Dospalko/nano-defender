367df3d577a5634142745b506921562e5498009e
Dominik Palo <dominik.palo@student.tuke.sk>
2025-07-31 15:15:51 +0200

    feat: implementing the powerups to game scene

--- src/objects/PowerUp.ts
+++ src/objects/PowerUp.ts
@@ -1,5 +1,18 @@
 import Phaser from "phaser";
+
+export type PowerType = "trip" | "speed" | "shield" | "heal";
+
 export default class PowerUp extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene: Phaser.Scene,x:number,y:number){ super(scene,x,y,"powerup"); scene.add.existing(this); scene.physics.add.existing(this); this.setRotation(0); }
-  update(_: Phaser.Types.Input.Keyboard.CursorKeys,dt:number){ this.rotation += 0.002*dt; }
+  readonly ptype: PowerType;
+
+  constructor(scene: Phaser.Scene, x: number, y: number, ptype: PowerType) {
+    super(scene, x, y, `power-${ptype}`);
+    this.ptype = ptype;
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+  }
+
+  update(_: number, dt: number) {
+    this.rotation += 0.002 * dt; // pomalá rotácia
+  }
 }
